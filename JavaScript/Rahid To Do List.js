25a0d1931b52419dba6b4b9dc7f4f0f840f99e1e
Rahid Uddin <rahiduddin@yahoo.com>
2017-08-02 13:45:55 -0400

    Updated code with a few revisions

--- JavaScript/Rahid To Do List.js
+++ JavaScript/Rahid To Do List.js
@@ -7,3 +7,22 @@ function Task1Complete(silentshane) {
     document.getElementById(silentshane).style.textDecoration = 'line-through';
   }
 }
+
+function clearlist() {
+  names = document.getElementByClassName("line-through");
+  for (i = 0; i < names, length; i++) {
+    names[i].style.textDecoration = "none";
+  }
+}
+
+function addnew() {
+  var addme = prompt("Add New");
+  addListItem(addme);
+}
+
+function addListItem(text) {
+  list = document.querySelector('ol');
+  item = document.createElement('li');
+  item.innerText = text;
+  list.appendChild(item);
+}
